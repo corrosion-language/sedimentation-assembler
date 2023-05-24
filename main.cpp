24609f06fd159b8d952a1516d0063d609bb82b60
wdotmathree <wdotmathree@gmail.com>
2023-05-24 15:36:28 -0600

    Remove windows workflow

--- main.cpp
+++ main.cpp
@@ -450,8 +450,6 @@ void process_instructions() {
 }
 
 int main(int argc, char *argv[]) {
-	std::cerr << "testcerr" << std::endl;
-	std::cout << "testcout" << std::endl;
 	if (parse_args(argc, argv))
 		return 1;
 
